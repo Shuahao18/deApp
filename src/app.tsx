5e755790f025dd2a902b96ee508f494764a20927
josh <joshuemeredores@gmail.com>
2025-05-26 23:27:51 +0800

    routing from the bottom

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,26 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import Dashboard from './pages/Dashboard';
-import Contribution from './pages/Contribution'; // ✅ Make sure this path is correct
-import { Sidebar } from 'lucide-react';
-import CalendarEvent from './pages/CalendarEvent';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Contribution from './pages/Contribution';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
 import ErrorHandler from './pages/Diva';
 
-
-const router= createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,
     errorElement: <ErrorHandler />,
     children: [
-    { path:"/dashboard",element:<Dashboard/>},
-    { path:"members/contribution",element:<Contribution/>},
+      { index: true, element: <Dashboard /> },
+      { path: "dashboard", element: <Dashboard /> },
+      { path: "members/contribution", element: <Contribution /> },
     ],
   },
 ]);
 
-
- ReactDOM.createRoot(document.getElementById('app')!).render(
- <StrictMode>
-  <RouterProvider router={router} />
- </StrictMode>
- );
- 
-
-
-
+ReactDOM.createRoot(document.getElementById('app')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
